034c5b0874c5fe4b6fe37743240064b5dadfbe8a
KhushiSavarn <khushisavarn003@gmail.com>
2024-04-02 00:23:34 +0530

    quiz-module

---
+++ app/src/main/java/DataItem.kt
@@ -0,0 +1,10 @@
+data class DataItem(
+    val answer1: String,
+    val answer2: String,
+    val answer3: String,
+    val answer4: String,
+    val correct: String,
+    val department: String,
+    val id: Int,
+    val question: String
+)
